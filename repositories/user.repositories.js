d301ee3e326b69245921c30980546177ef587d65
LeTonVinh <le.tonvinh1@gmail.com>
2025-06-14 16:23:50 +0700

    create user reponsitories for data

---
+++ repositories/user.repositories.js
@@ -0,0 +1,15 @@
+import User from '../models/user.model.js';
+
+const findByEmailOrPhone = async (email, phone) => {
+  return await User.findOne({ $or: [{ email }, { phone }] });
+};
+
+const createUser = async (userData) => {
+  return await User.create(userData);
+};
+
+const findByPhone = async (phone) => {
+  return await User.findOne({ phone });
+};
+
+export default { findByEmailOrPhone, createUser, findByPhone };
