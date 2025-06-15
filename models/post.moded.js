282be9f9cc5ccf23d38a05ec02ca4c4ee3b4ce2d
LeTonVinh <le.tonvinh1@gmail.com>
2025-06-15 10:46:44 +0700

    Add post model schema for managing posts in the application

---
+++ models/post.moded.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  images: [String],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  likes: [ { type: mongoose.Schema.Types.ObjectId, ref: 'User' } ],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  privacy: { type: String, enum: ['public', 'follower', 'private'], default: 'public' },
+  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  status: { type: String, enum: ['active', 'deleted', 'hidden'], default: 'active' }
+});
+
+export default mongoose.model('Post', postSchema);
