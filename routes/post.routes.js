1fbe4f9c40e4559cc743f3c84e28ad234d3e7378
LeTonVinh <le.tonvinh1@gmail.com>
2025-06-20 19:36:06 +0700

    cần xác thực khi lấy  tất cả các bài viết

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -6,30 +6,30 @@ const router = express.Router();
 
 // Đăng bài viết mới
 router.route('/')
-  .post(authMiddleware, postController.createPost) // Tạo bài viết
-  .get(postController.getAllPosts); // Lấy tất cả bài viết
+    .post(authMiddleware, postController.createPost) // Tạo bài viết
+    .get(authMiddleware, postController.getAllPosts); // Lấy tất cả bài viết
 // Láy tất cả bài viết của tôi
 router.route('/my')
-  .get(authMiddleware, postController.getMyPosts); // Lấy tất cả bài viết của tôi 
+    .get(authMiddleware, postController.getMyPosts); // Lấy tất cả bài viết của tôi 
 
 // Sửa, xóa bài viết theo id
 router.route('/:id')
-  .put(authMiddleware, postController.updatePost) // Sửa bài viết
-  .delete(authMiddleware, postController.deletePost) // Xóa bài viết
-  .get(authMiddleware,postController.getPostById); // Lấy bài viết theo id
+    .put(authMiddleware, postController.updatePost) // Sửa bài viết
+    .delete(authMiddleware, postController.deletePost) // Xóa bài viết
+    .get(authMiddleware, postController.getPostById); // Lấy bài viết theo id
 // Like/Unlike bài viết
 router.route('/:id/like')
-  .post(authMiddleware, postController.likePost);
+    .post(authMiddleware, postController.likePost);
 
 router.route('/:id/unlike')
-  .post(authMiddleware, postController.unlikePost);
+    .post(authMiddleware, postController.unlikePost);
 
 // View bài viết
 router.route('/:id/view')
-  .post(authMiddleware, postController.viewPost);
+    .post(authMiddleware, postController.viewPost);
 
 // Share bài viết
 router.route('/:id/share')
-  .post(authMiddleware, postController.sharePost);
+    .post(authMiddleware, postController.sharePost);
 
 export default router;
